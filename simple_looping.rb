090aa40bed57bd6376129e9d4c0bff0db204e70f
Matthew Bartl <mattbartl@gmail.com>
2019-03-19 00:49:48 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -54,7 +54,7 @@ def for_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   range = 1..7
-  for ahit in range
+  for number in range
     puts phrase
   end
   
